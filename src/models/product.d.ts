76273207d137da1ca346344842d4cb6c3c575423
Alesia <alesia.litvinenko@gmail.com>
2022-12-13 23:38:10 +0300

    feat: edit Interface Product

--- src/models/product.d.ts
+++ src/models/product.d.ts
@@ -1,3 +1,13 @@
 interface Product {
-    [key: string]: string;
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
 }
