aab8255da42d881be291382530e9043d259dfe13
lgklsv <kolosov20011@gmail.com>
2022-12-14 12:13:26 +0300

    feat: add route paths and basic templates

---
+++ src/const/route-paths.ts
@@ -0,0 +1,18 @@
+export const paths = {
+  404: {
+    template: "../pages/404.html",
+    title: "404 | Online Store"
+  },
+  "/": {
+    template: "../pages/main.html",
+    title: "Online Store"
+  },
+  "/cart": {
+    template: "../pages/cart.html",
+    title: "Cart | Online Store"
+  },
+  "/product": {
+    template: "../pages/product.html",
+    title: "Product | Online Store"
+  }
+}
