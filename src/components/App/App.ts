31e8f2b187e6d44ec006521f03caef4828665b1a
Alesia <alesia.litvinenko@gmail.com>
2022-12-15 19:33:17 +0300

    feat: add module scss for webpack

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,20 +1,31 @@
-import styles from './App.scss';
+import styles from './App.module.scss';
 
 export const renderApp = () => {
     // накидываем стиль на body
     document.body.classList.add(styles['body']);
-
+    console.log(styles);
     // #app - точка входа в разметке html
     // накидываем стиль на контейнер
     const appContiner: Element = document.querySelector('#app') as Element;
     appContiner.classList.add(styles['container']);
 
+    const birdMenuList = document.createElement('ul');
+    // birdMenuList.classList.add('bird-menu__list');
+
+    // SCORE
+    for (let i = 0; i < 4; i++) {
+        const birdMenuItem = document.createElement('li');
+        // birdMenuItem.classList.add('bird-menu__item');
+
+        const itemLink = document.createElement('a');
+        // itemLink.classList.add('item_link');
+        itemLink.innerHTML = `aaaaaaaaaaaaaaaaaaaaaaaa}`;
+
+        birdMenuItem.append(itemLink);
+        birdMenuList.append(birdMenuItem);
+    }
+
     // const header = renderHeader();
 
-    // appContiner.append(
-    //     header
-    //     // main,
-    //     // footer,
-    //     // button,
-    // );
+    appContiner.append(birdMenuList);
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -47,7 +47,18 @@ const config: Configuration = {
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
                 // options: { module: true },
             },
             {
