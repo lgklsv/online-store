80d1efe17deb9905800f504822ab6cd2c42b8f5b
lgklsv <kolosov20011@gmail.com>
2022-12-16 23:43:50 +0300

    feat: add a method to create basic link elements

--- src/components/Footer/Footer.ts
+++ src/components/Footer/Footer.ts
@@ -1,42 +1,39 @@
-import { createElem } from "../../utils/create-element";
+import { createElem } from '../../utils/create-element';
+import { createLink } from '../../utils/create-link-element';
 
 export const renderFooter = (): HTMLElement => {
     const footer: HTMLElement = createElem('footer', 'footer');
-
     const footerContainer: HTMLElement = createElem('div', 'footer__container');
 
     // credentials (top)
     const footerCredentials: HTMLElement = createElem('div', 'footer__credentials');
     footerCredentials.innerHTML = 'markett engineered by ';
 
-    const footerGitLink: HTMLElement = createElem('a', 'footer__link');
-    footerGitLink.setAttribute('href', 'https://github.com/Alesia-Abaeva');
-    footerGitLink.setAttribute('target', '_blank');
-    footerGitLink.innerHTML = 'Alesia-Abaeva';
+    const footerGitLink: HTMLElement = createLink(
+        'https://github.com/Alesia-Abaeva',
+        'footer__link',
+        true,
+        'Alesia-Abaeva'
+    );
 
     footerCredentials.append(footerGitLink);
     footerCredentials.innerHTML = footerCredentials.innerHTML + ' & ';
 
-    const footerGitLink2: HTMLElement = createElem('a', 'footer__link');
-    footerGitLink2.setAttribute('href', 'https://github.com/lgklsv');
-    footerGitLink2.setAttribute('target', '_blank');
-    footerGitLink2.innerHTML = 'lgklsv';
+    const footerGitLink2: HTMLElement = createLink('https://github.com/lgklsv', 'footer__link', true, 'lgklsv');
 
     footerCredentials.append(footerGitLink2);
 
     // copyright (bottom)
     const footerCopyright: HTMLElement = createElem('div', 'footer__copyright');
 
-    const rsSchoolLink: HTMLElement = createElem('a', 'footer__link');
-    footerGitLink2.setAttribute('href', 'https://rs.school/js/');
-    footerGitLink2.setAttribute('target', '_blank');
+    const rsSchoolLink: HTMLElement = createLink('https://rs.school/js/', 'footer__link', true, '');
 
     const copyrightText: HTMLElement = createElem('p', 'footer__text');
     copyrightText.innerHTML = '© 2022 markett. All rights reserved.';
-    
-    footerCopyright.append(rsSchoolLink, copyrightText)
+
+    footerCopyright.append(rsSchoolLink, copyrightText);
 
     footerContainer.append(footerCredentials, footerCopyright);
     footer.append(footerContainer);
     return footer;
-}
+};
