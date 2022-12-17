11c7f9c2dae347f263df5a441e8a5ea07ba5af85
Alesia <alesia.litvinenko@gmail.com>
2022-12-17 22:52:27 +0300

    feat: add event on mouse

--- src/components/PageMain/components/MainToolbar/MainToolbar.ts
+++ src/components/PageMain/components/MainToolbar/MainToolbar.ts
@@ -3,16 +3,16 @@ import { createElem } from '../../../../utils/create-element';
 import styles from './MainToolbar.module.scss';
 
 export const renderMainToolbar = (): HTMLElement => {
-    // sortOptions;
     const mainToolbar: HTMLElement = createElem('div', 'main__toolbar');
 
+    // toolbar;
     const toolbarSelect: HTMLElement = createElem('div', styles['toolbar__select_item-sort']);
     const selectItem: HTMLElement = createElem('div', styles['select_item-sort']);
     const itemSpan: HTMLElement = createElem('span', styles['select_item-sort__text']);
     itemSpan.innerHTML = 'Сортировка';
 
     const selectDrop: HTMLElement = createElem('div', styles['select_dropdown']);
-    const selectList: HTMLElement = createElem('li', styles['select_list']);
+    const selectList: HTMLElement = createElem('ul', styles['select_list']);
 
     for (let i = 0; i < sortOptions.length; i++) {
         const selectItem = createElem('li', 'select__item');
@@ -23,8 +23,16 @@ export const renderMainToolbar = (): HTMLElement => {
     selectDrop.append(selectList);
 
     selectItem.append(itemSpan);
-    toolbarSelect.append(selectItem);
-    mainToolbar.append(toolbarSelect, selectDrop);
+    toolbarSelect.append(selectItem, selectDrop);
+    mainToolbar.append(toolbarSelect);
+
+    selectItem.onmouseenter = () => {
+        selectDrop.classList.add(styles['open']);
+    };
+
+    toolbarSelect.onmouseleave = () => {
+        selectDrop.classList.remove(styles['open']);
+    };
 
     return mainToolbar;
 };
