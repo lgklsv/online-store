fa3867fc755b31005b647a25b6f629b2b7589cd9
Alesia <alesia.litvinenko@gmail.com>
2022-12-18 00:42:07 +0300

    refactor: optimization structure in maintoolbar

--- src/components/PageMain/components/MainToolbar/MainToolbar.ts
+++ src/components/PageMain/components/MainToolbar/MainToolbar.ts
@@ -1,55 +1,13 @@
-import { sortOptions } from '../../../../const/select-sort';
 import { createElem } from '../../../../utils/create-element';
+import { renderSelectSort } from './components/SelectSort/SelectSort';
 import styles from './MainToolbar.module.scss';
 
 export const renderMainToolbar = (): HTMLElement => {
-    const mainToolbar: HTMLElement = createElem('div', 'main__toolbar');
+    const mainToolbar: HTMLElement = createElem('div', styles['main__toolbar']);
 
-    // select-sort
-    const toolbarSelect: HTMLElement = createElem('div', styles['toolbar__select_item-sort']);
+    const selectSort: HTMLElement = renderSelectSort();
 
-    const selectItem: HTMLElement = createElem('div', styles['select_item-sort']);
-    const itemSpan: HTMLElement = createElem('span', styles['select_item-sort__text']);
-    itemSpan.innerHTML = 'Сортировка';
-
-    const arrowButton: HTMLElement = createElem('div', 'select__arrow');
-
-    const selectDrop: HTMLElement = createElem('div', styles['select_dropdown']);
-    const selectList: HTMLElement = createElem('ul', styles['select_list']);
-    const itemli: HTMLElement[] = [];
-
-    for (let i = 0; i < sortOptions.length; i++) {
-        const selectItemLi = createElem('li', 'select__item');
-        selectItemLi.innerHTML = sortOptions[i];
-        selectList.append(selectItemLi);
-        itemli.push(selectItemLi);
-
-        selectItemLi.onclick = () => {
-            // TODO - оптимизировать и убрать двойной цикл
-            itemli.forEach((el) => {
-                el.classList.remove('select');
-            });
-
-            selectItemLi.classList.add('select');
-            itemSpan.innerHTML = selectItemLi.textContent as string;
-        };
-    }
-
-    selectDrop.append(selectList);
-
-    selectItem.append(itemSpan, arrowButton);
-    toolbarSelect.append(selectItem, selectDrop);
-    mainToolbar.append(toolbarSelect);
-
-    selectItem.onmouseenter = () => {
-        selectDrop.classList.add(styles['open']);
-        arrowButton.classList.add('active-arr');
-    };
-
-    toolbarSelect.onmouseleave = () => {
-        selectDrop.classList.remove(styles['open']);
-        arrowButton.classList.remove('active-arr');
-    };
+    mainToolbar.append(selectSort);
 
     return mainToolbar;
 };
