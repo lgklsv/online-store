682143135f64c9056857ee6ffc100a256ec614c1
Alesia <alesia.litvinenko@gmail.com>
2022-12-18 15:35:56 +0300

    feat: add product quantity

--- src/components/PageMain/components/MainToolbar/MainToolbar.ts
+++ src/components/PageMain/components/MainToolbar/MainToolbar.ts
@@ -1,3 +1,4 @@
+import { PRODUCTS } from '../../../../const/products';
 import { createElem } from '../../../../utils/create-element';
 import { renderSelectSort } from './components/SelectSort/SelectSort';
 import styles from './MainToolbar.module.scss';
@@ -6,8 +7,10 @@ export const renderMainToolbar = (): HTMLElement => {
     const mainToolbar: HTMLElement = createElem('div', styles['main__toolbar']);
 
     const selectSort: HTMLElement = renderSelectSort();
+    const productQuantity: HTMLElement = createElem('div', styles['toolbar__quantity']);
+    productQuantity.innerHTML = `FOUND: ${String(PRODUCTS.length)}`;
 
-    mainToolbar.append(selectSort);
+    mainToolbar.append(productQuantity, selectSort);
 
     return mainToolbar;
 };
