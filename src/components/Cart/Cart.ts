2780f369bd8bc4772ef45c0ee0e7ae90bbafd3aa
lgklsv <kolosov20011@gmail.com>
2022-12-19 18:35:55 +0300

    feat: add and style checkout section, add some css variables

--- src/components/Cart/Cart.ts
+++ src/components/Cart/Cart.ts
@@ -1,5 +1,6 @@
 import { createElem } from '../../utils/create-element';
 import styles from './Cart.module.scss';
+import { renderCartCheckout } from './CartCheckout/CartCheckout';
 import { renderCartItems } from './CartItems/CartItems';
 
 export const renderCartPage = (): HTMLElement => {
@@ -7,17 +8,23 @@ export const renderCartPage = (): HTMLElement => {
     const mainContainer: HTMLElement = createElem('div', 'main__container');
     const mainContent: HTMLElement = createElem('div', styles['cart']);
 
-    const cartItemsContainer: HTMLElement = createElem('div', 'cart__items-container');
-    const cartCheckoutContainer: HTMLElement = createElem('div', 'cart__checkout-container');
-
+    
     // Cart items (left part)
+    const cartItemsContainer: HTMLElement = createElem('div', 'cart__items-container');
     const cartHeading: HTMLElement = createElem('h1', 'cart__heading');
     cartHeading.innerHTML = 'Товары в корзине';
     const cartItems: HTMLElement = renderCartItems();
 
     cartItemsContainer.append(cartHeading, cartItems);
 
+
     // Cart checkout (right part)
+    const cartCheckoutContainer: HTMLElement = createElem('div', 'cart__checkout-container');
+    const checkoutHeading: HTMLElement = createElem('h1', 'cart__heading');
+    checkoutHeading.innerHTML = 'Итого';
+    const cartCheckout: HTMLElement = renderCartCheckout();
+
+    cartCheckoutContainer.append(checkoutHeading, cartCheckout)
 
     mainContent.append(cartItemsContainer, cartCheckoutContainer);
     mainContainer.append(mainContent);
