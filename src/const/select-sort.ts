b2e98c2ae6037fa7afc89740fcfedc94758d7e97
Alesia <alesia.litvinenko@gmail.com>
2022-12-23 18:06:32 +0300

    feat: add raiting on products card in mane page & edit sort functions

--- src/const/select-sort.ts
+++ src/const/select-sort.ts
@@ -6,7 +6,7 @@ export const SORT_TYPE = {
     PRICES_ASC: 'PRICES_ASC',
     PRICES_DESC: 'PRICES_DESC',
     DISCOUNT_ASC: 'DISCOUNT_ASC',
-    DISCOUNT_DESC: 'DISCOUNT_DESC',
+    RATING: 'RATING',
 } as const;
 
 /** Опции сортировки для отрисовки в разметке */
@@ -14,8 +14,8 @@ export const SORT_OPTIONS: Record<SortTypes, string> = {
     [SORT_TYPE.DEFAULT]: 'По умолчанию',
     [SORT_TYPE.PRICES_ASC]: 'По возрастанию цены',
     [SORT_TYPE.PRICES_DESC]: 'По убыванию цены',
-    [SORT_TYPE.DISCOUNT_ASC]: 'По возрастанию скидки',
-    [SORT_TYPE.DISCOUNT_DESC]: 'По убыванию скидки',
+    [SORT_TYPE.DISCOUNT_ASC]: 'По размеру скидки',
+    [SORT_TYPE.RATING]: 'По рейтингу',
 };
 
 /** Функции сортировки с соответствием типу сортировки через идентификатор */
@@ -23,8 +23,8 @@ export const SORT_FUNCTIONS: Record<SortTypes, SortFn> = {
     [SORT_TYPE.DEFAULT]: (products) => quickSort(products, 'id'),
     [SORT_TYPE.PRICES_ASC]: (products) => quickSort(products, 'discountPrice'),
     [SORT_TYPE.PRICES_DESC]: (products) => quickSort(products, 'discountPrice').reverse(),
-    [SORT_TYPE.DISCOUNT_ASC]: (products) => quickSort(products, 'rating'),
-    [SORT_TYPE.DISCOUNT_DESC]: (products) => quickSort(products, 'rating').reverse(),
+    [SORT_TYPE.DISCOUNT_ASC]: (products) => quickSort(products, 'discountPercentage').reverse(),
+    [SORT_TYPE.RATING]: (products) => quickSort(products, 'rating').reverse(),
 };
 
 /** массив с вариантами сортировки */
