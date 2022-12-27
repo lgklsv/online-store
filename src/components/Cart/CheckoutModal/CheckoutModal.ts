28eb7da4cbf85d765f80c423eb365af51767172c
lgklsv <kolosov20011@gmail.com>
2022-12-27 22:04:50 +0300

    refactor: split code to separate functions(handlers, input fields creator function

--- src/components/Cart/CheckoutModal/CheckoutModal.ts
+++ src/components/Cart/CheckoutModal/CheckoutModal.ts
@@ -1,15 +1,10 @@
 import { createElem } from '../../../utils/create-element';
-import { createInput } from '../../../utils/create-input-element';
 import styles from './CheckoutModal.module.scss';
 import { renderCard } from './components/CreditCard/CreditCard';
 import { toggleModal } from './components/ToggleModal';
-import { validateAddress } from './components/Validators/validateAddress';
-import { validateEmail } from './components/Validators/validateEmail';
-import { validateName } from './components/Validators/validateName';
-import { validatePhone } from './components/Validators/validateTel';
-import { validateCardNumber } from './components/Validators/validateCardNumber';
-import { validateExpDate } from './components/Validators/validateExpDate';
-import { validateCvc } from './components/Validators/validateCvc';
+import { onNameInput, onEmailInput, onPhoneInput, onAddressInput } from './components/onInputCheckout';
+import { onSubmitCheckout } from './components/submitHandler';
+import { createInputModal } from './components/createInputModal';
 
 export const renderCheckoutModal = (): HTMLElement => {
     const modal: HTMLElement = createElem('div', styles['checkout-modal']);
@@ -33,98 +28,12 @@ export const renderCheckoutModal = (): HTMLElement => {
 
     const checkoutDetails: HTMLElement = createElem('div', 'checkout-modal__details');
 
-    // NAME
-    const nameDetails: HTMLElement = createElem('div', 'checkout-modal__input-details');
-    const nameLabel: HTMLElement = createElem('label', 'checkout-modal__label');
-    nameLabel.innerHTML = 'Имя и фамилия';
+    const nameInput = createInputModal('Имя и фамилия', 'Введите имя и фамилию', 'name', onNameInput);
+    const emailInput = createInputModal('E-mail', 'Введите e-mail', 'email', onEmailInput);
+    const phoneInput = createInputModal('Номер телефона', 'Введите номер телефона', 'phone', onPhoneInput);
+    const addressInput = createInputModal('Адрес', 'Введите адрес', 'address', onAddressInput);
 
-    const nameInput: HTMLInputElement = createInput('text', 'checkout-modal__input');
-    nameInput.setAttribute('placeholder', 'Введите имя и фамилию');
-    nameInput.setAttribute('spellcheck', 'false');
-
-    nameInput.oninput = (e: Event): void => {
-        if (e.target instanceof HTMLInputElement) {
-            const inputValue = e.target.value.trim();
-            const messageElement = e.target.nextElementSibling as HTMLElement;
-            validateName(inputValue, messageElement);
-        }
-    };
-
-    const nameError: HTMLElement = createElem('p', 'checkout-modal__message');
-    nameError.innerHTML = 'Эти данные необходимы для получения и оформления заказа';
-
-    nameDetails.append(nameLabel, nameInput, nameError);
-
-    // EMAIL
-    const emailDetails: HTMLElement = createElem('div', 'checkout-modal__input-details');
-    const emailLabel: HTMLElement = createElem('label', 'checkout-modal__label');
-    emailLabel.innerHTML = 'E-mail';
-
-    const emailInput: HTMLInputElement = createInput('email', 'checkout-modal__input');
-    emailInput.setAttribute('placeholder', 'Введите e-mail');
-    emailInput.setAttribute('spellcheck', 'false');
-
-    emailInput.oninput = (e: Event): void => {
-        if (e.target instanceof HTMLInputElement) {
-            const inputValue = e.target.value.trim();
-            const messageElement = e.target.nextElementSibling as HTMLElement;
-            validateEmail(inputValue, messageElement);
-        }
-    };
-    const emailError: HTMLElement = createElem('p', 'checkout-modal__message');
-    emailError.innerHTML = 'Эти данные необходимы для получения и оформления заказа';
-
-    emailDetails.append(emailLabel, emailInput, emailError);
-
-    // PHONE
-    const phoneDetails: HTMLElement = createElem('div', 'checkout-modal__input-details');
-    const phoneLabel: HTMLElement = createElem('label', 'checkout-modal__label');
-    phoneLabel.innerHTML = 'Номер телефона';
-
-    const phoneInput: HTMLInputElement = createInput('tel', 'checkout-modal__input');
-    phoneInput.setAttribute('placeholder', 'Введите номер телефона');
-    phoneInput.setAttribute('spellcheck', 'false');
-
-    phoneInput.oninput = (e: Event): void => {
-        if (e.target instanceof HTMLInputElement) {
-            const inputValue = e.target.value.trim();
-            if (inputValue.length === 1 && inputValue !== '+') {
-                e.target.value = `+${e.target.value}`;
-            }
-            if (inputValue.length > 18) e.target.value = inputValue.slice(0, 18);
-            const messageElement = e.target.nextElementSibling as HTMLElement;
-            validatePhone(inputValue, messageElement);
-        }
-    };
-
-    const phoneError: HTMLElement = createElem('p', 'checkout-modal__message');
-    phoneError.innerHTML = 'Эти данные необходимы для получения и оформления заказа';
-
-    phoneDetails.append(phoneLabel, phoneInput, phoneError);
-
-    // ADRESS
-    const addressDetails: HTMLElement = createElem('div', 'checkout-modal__input-details');
-    const addressLabel: HTMLElement = createElem('label', 'checkout-modal__label');
-    addressLabel.innerHTML = 'Адрес';
-
-    const addressInput: HTMLInputElement = createInput('text', 'checkout-modal__input');
-    addressInput.setAttribute('placeholder', 'Введите адрес');
-    addressInput.setAttribute('spellcheck', 'false');
-
-    addressInput.oninput = (e: Event): void => {
-        if (e.target instanceof HTMLInputElement) {
-            const inputValue = e.target.value.trim();
-            const messageElement = e.target.nextElementSibling as HTMLElement;
-            validateAddress(inputValue, messageElement);
-        }
-    };
-
-    const addressError: HTMLElement = createElem('p', 'checkout-modal__message');
-    addressError.innerHTML = 'Эти данные необходимы для получения и оформления заказа';
-
-    addressDetails.append(addressLabel, addressInput, addressError);
-
-    checkoutDetails.append(nameDetails, emailDetails, phoneDetails, addressDetails);
+    checkoutDetails.append(nameInput, emailInput, phoneInput, addressInput);
 
     const paymentBtn: HTMLElement = createElem('button', 'checkout-modal__confirm-btn');
     paymentBtn.setAttribute('type', 'submit');
@@ -136,75 +45,6 @@ export const renderCheckoutModal = (): HTMLElement => {
     checkoutForm.append(checkoutDetails, card, paymentBtn);
     modal.append(heading, closeModalBtn, checkoutForm);
 
-    checkoutForm.onsubmit = (e: Event): void => {
-        e.preventDefault();
-
-        const name = nameInput.value;
-        const messageName = nameInput.nextElementSibling as HTMLElement;
-
-        const email = emailInput.value;
-        const messageEmail = emailInput.nextElementSibling as HTMLElement;
-
-        const phone = phoneInput.value;
-        const messagePhone = phoneInput.nextElementSibling as HTMLElement;
-
-        const address = addressInput.value;
-        const messageAddress = addressInput.nextElementSibling as HTMLElement;
-
-        const cardNumInput = document.getElementById('card-number') as HTMLInputElement;
-        let cardNum = cardNumInput.value;
-        cardNum = cardNum.replace(/ /g, '');
-        let paymentSystem = validateCardNumber(cardNum);
-
-        const cardExpInput = document.getElementById('expiration') as HTMLInputElement;
-        let expNum = cardExpInput.value;
-
-        const cardCvcInput = document.getElementById('cvc-cvv') as HTMLInputElement;
-        let cardCvc = cardCvcInput.value;
-
-        if (
-            validateName(name, messageName) &&
-            validateEmail(email, messageEmail) &&
-            validatePhone(phone, messagePhone) &&
-            validateAddress(address, messageAddress) &&
-            validateCardNumber(cardNum) &&
-            validateExpDate(expNum) &&
-            validateCvc(paymentSystem, cardCvc)
-        ) {
-            // TODO clean cart
-
-
-            // render success page
-            modal.innerHTML = '';
-            const heading: HTMLElement = createElem('h2', 'checkout-modal__heading');
-            heading.style.marginTop = '2rem';
-            heading.innerHTML = 'Спасибо за заказ ヽ(•‿•)ノ На главную страницу через 3 сек';
-            modal.append(heading);
-
-            let time = 3;
-            setInterval((): void => {
-                time--;
-                heading.innerHTML = `Спасибо за заказ ヽ(•‿•)ノ На главную страницу через ${time} сек`;
-                // return to main
-                if (time === 0) window.location.href = '/';
-            }, 1000);
-        } else {
-            if (!validateName(name, messageName)) nameError.classList.add('error');
-            if (!validateEmail(email, messageEmail)) emailError.classList.add('error');
-            if (!validatePhone(phone, messagePhone)) phoneError.classList.add('error');
-            if (!validateAddress(address, messageAddress)) addressError.classList.add('error');
-            if (!validateCardNumber(cardNum)) cardNumInput.classList.add('error');
-            if (!validateExpDate(expNum)) cardExpInput.classList.add('error');
-            if (!validateCvc(paymentSystem, cardCvc)) cardCvcInput.classList.add('error');
-
-            if (!validateCardNumber(cardNum) || !validateExpDate(expNum) || !validateCvc(paymentSystem, cardCvc)) {
-                // Animation
-                const cardEl = card.firstElementChild as HTMLElement;
-                cardEl.classList.remove('card__error-ani');
-                void cardEl.offsetWidth; // trigger a DOM reflow
-                cardEl.classList.add('card__error-ani');
-            }
-        }
-    };
+    checkoutForm.onsubmit = onSubmitCheckout;
     return modal;
 };
--- src/components/Cart/CheckoutModal/components/CreditCard/CreditCard.ts
+++ src/components/Cart/CheckoutModal/components/CreditCard/CreditCard.ts
@@ -1,8 +1,7 @@
+import styles from './CreditCard.module.scss';
 import { createElem } from '../../../../../utils/create-element';
 import { createInput } from '../../../../../utils/create-input-element';
-import { validateCardNumber } from '../Validators/validateCardNumber';
-import { validateExpDate } from '../Validators/validateExpDate';
-import styles from './CreditCard.module.scss';
+import { onCardNumberInput, onCvcInput, onExpDateInput } from './components/onInputCreditCard';
 
 export const renderCard = (): HTMLElement => {
     // CARD
@@ -19,32 +18,7 @@ export const renderCard = (): HTMLElement => {
     cardNumInput.id = 'card-number';
     cardNumInput.setAttribute('placeholder', '0000 0000 0000 0000');
 
-    let paymentSystem: string | boolean;
-    cardNumInput.oninput = (e: Event): void => {
-        const target = e.target as HTMLInputElement;
-        target.classList.remove('error');
-        card.classList.remove('card__error-ani');
-        let inputValue = target.value;
-
-        target.value = inputValue
-            .replace(/[^\d]+/g, '')
-            .replace(/\W/gi, '')
-            .replace(/(.{4})/g, '$1 ')
-            .trim();
-        if (inputValue.length > 19) target.value = inputValue.slice(0, 19);
-
-        if (inputValue[0] === '4') {
-            logo.className = 'card__logo card__logo_mastercard';
-        } else if (inputValue[0] === '5') {
-            logo.className = 'card__logo card__logo_visa';
-        } else if (inputValue.slice(0, 2) === '34' || inputValue.slice(0, 2) === '37') {
-            logo.className = 'card__logo card__logo_amex';
-        } else {
-            logo.className = 'card__logo';
-        }
-        validateCardNumber(inputValue);
-    };
-
+    cardNumInput.oninput = onCardNumberInput;
     cardNum.append(cardNumLabel, cardNumInput);
 
     // Bottom
@@ -59,31 +33,7 @@ export const renderCard = (): HTMLElement => {
     cardExpInput.setAttribute('placeholder', 'ММ / ГГ');
     cardExpInput.id = 'expiration';
 
-    cardExpInput.oninput = (e: Event): void => {
-        const target = e.target as HTMLInputElement;
-        target.classList.remove('error');
-
-        let inputValue = target.value;
-
-        target.value = inputValue
-            .replace(/^([1-9]\/|[2-9])$/g, '0$1/')
-            .replace(/^(0[1-9]|1[0-2])$/g, '$1/')
-            .replace(/^([0-1])([3-9])$/g, '0$1/$2')
-            .replace(/^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2')
-            .replace(/^([0]+)\/|[0]+$/g, '0')
-            .replace(/[^\d\/]|^[\/]*$/g, '')
-            .replace(
-                /\//g,
-                (
-                    (i) => (m: string) =>
-                        !i++ ? m : ''
-                )(0)
-            )
-            .trim();
-        if (inputValue.length > 5) target.value = inputValue.slice(0, 5);
-        validateExpDate(inputValue);
-    };
-
+    cardExpInput.oninput = onExpDateInput;
     cardExp.append(cardExpLabel, cardExpInput);
 
     // CVC
@@ -96,30 +46,7 @@ export const renderCard = (): HTMLElement => {
     cardCvcInput.id = 'cvc-cvv';
     cardCvc.append(cardCvcLabel, cardCvcInput);
 
-    cardCvcInput.oninput = (e: Event): void => {
-        const target = e.target as HTMLInputElement;
-        target.classList.remove('error');
-
-        let inputValue = target.value;
-
-        target.value = inputValue
-            .replace(
-                /[^\d]/g,
-                ''
-            )
-            .trim();
-            
-        const cardNumInput = document.getElementById('card-number') as HTMLInputElement;
-        let cardNum = cardNumInput.value;
-        cardNum = cardNum.replace(/ /g, '');
-        paymentSystem = validateCardNumber(cardNum);
-
-        if (paymentSystem === 'amex') {
-            if (inputValue.length > 4) target.value = inputValue.slice(0, 4);
-        } else {
-            if (inputValue.length > 3) target.value = inputValue.slice(0, 3);
-        }
-    };
+    cardCvcInput.oninput = onCvcInput;
 
     cardDateAndCVC.append(cardExp, cardCvc);
 
