72ac9944d30fa7e6cba924a26ca66edcd9b3212d
lgklsv <kolosov20011@gmail.com>
2022-12-28 20:37:44 +0300

    refactor: remove link element on cart icon and make all field clickable

--- src/components/Header/components/HeaderCart/HeaderCart.ts
+++ src/components/Header/components/HeaderCart/HeaderCart.ts
@@ -5,27 +5,16 @@ import { router } from '../../../../utils/router';
 
 export const renderHeaderCart = (): HTMLElement => {
     const containerCart: HTMLElement = createElem('div', styles['header__cart']);
+    const cartIcon: HTMLElement = createElem('div', 'header__cart-icon');
 
-    const cartWrapper: HTMLElement = createElem('div', 'header__cart_container');
-    const cartLink: HTMLElement = createElem('a', styles['header_cart-link']);
-    cartLink.setAttribute('href', '/cart');
-
-    cartLink.onclick = (e: Event): void => {
-        e.preventDefault();
-        const target = e.target as HTMLAnchorElement;
-        const link = target.getAttribute('href');
-        if (link) router(link);
-    };
+    containerCart.onclick = (): void => router('/cart');
 
     const cartCounter: HTMLElement = createElem('div', styles['header_cart-counter']);
     const counterProduct: HTMLElement = createElem('span', styles['cart-counter']);
     counterProduct.innerHTML = String(productsCartData.count);
 
-    cartWrapper.append(cartLink);
-
     cartCounter.append(counterProduct);
 
-    containerCart.append(cartWrapper, cartCounter);
-
+    containerCart.append(cartIcon, cartCounter);
     return containerCart;
 };
