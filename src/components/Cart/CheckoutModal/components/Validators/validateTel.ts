186732b25b865bc683d6ed82da2ca55a055214f3
lgklsv <kolosov20011@gmail.com>
2023-01-01 18:35:49 +0300

    feat: inprove phone validation according to the task(at least 9 numbers) and add new error messages

--- src/components/Cart/CheckoutModal/components/Validators/validateTel.ts
+++ src/components/Cart/CheckoutModal/components/Validators/validateTel.ts
@@ -1,8 +1,15 @@
 export const validatePhone = (phone: string, messageEl: HTMLElement): boolean => {
-    if (/^(\+|00)[7-8][0-9 \-\(\)\.]{7,18}$/.test(phone)) {
+    const cleanPhone = phone.replace(/\D+/g, '');
+    if (cleanPhone.length >= 9 && /^(\+|00)[7-8][0-9 \-\(\)\.]{8,18}$/.test(phone)) {
         messageEl.innerHTML = '✓';
         messageEl.className = 'checkout-modal__message done';
         return true;
+    } else if (!cleanPhone[0].match(/[7-8]/)) {
+        messageEl.innerHTML = 'Номера телефонов могут начинаться только с +7 или +8';
+        messageEl.className = 'checkout-modal__message warning';
+    } else if (cleanPhone.length < 9) {
+        messageEl.innerHTML = 'Номер телефона должен быть не короче 9 цифр';
+        messageEl.className = 'checkout-modal__message warning';
     } else {
         messageEl.innerHTML = 'Номера телефонов могут начинаться только с +7 или +8';
         messageEl.className = 'checkout-modal__message warning';
