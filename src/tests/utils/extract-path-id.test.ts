def9fdf4554b0b33c9d29949522502622b3764b1
lgklsv <kolosov20011@gmail.com>
2023-01-07 18:22:31 +0300

    feat: add tests for address validation fucntion

--- src/tests/utils/extract-path-id.test.ts
+++ src/tests/utils/extract-path-id.test.ts
@@ -3,10 +3,13 @@ import { extractPathId } from '../../utils/extract-path-id';
 describe('extractPathId', () => {
     it('should return path id', () => {
         expect(extractPathId('/product/1')).toBe(1);
+        expect(extractPathId('/product/43')).toBe(43);
+        expect(extractPathId('/product/23432')).toBe(23432);
     });
     it('should return -1 if there is no id or id is not valid', () => {
         expect(extractPathId('/product/ytfyc')).toBe(-1);
         expect(extractPathId('/product/')).toBe(-1);
+        expect(extractPathId('')).toBe(-1);
     });
     it('should return -1 if path is not /product/:id', () => {
         expect(extractPathId('/product/ytfyc/1')).toBe(-1);
---
+++ src/tests/validators/validateAddress.test.ts
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { validateAddress } from '../../components/Cart/CheckoutModal/components/Validators/validateAddress';
+import { createElem } from '../../utils/create-element';
+
+describe('validateAddress', () => {
+    it('should return false if address is less than 3 words', () => {
+        expect(validateAddress('город', createElem('div', 'test'))).toBeFalsy();
+        expect(validateAddress('городмосква', createElem('div', 'test'))).toBeFalsy();
+        expect(validateAddress('город москва', createElem('div', 'test'))).toBeFalsy();
+        expect(validateAddress('', createElem('div', 'test'))).toBeFalsy();
+    });
+    it('should return false if there is less than 3 words with 5 characters', () => {
+        expect(validateAddress('город Уфа дом 3', createElem('div', 'test'))).toBeFalsy();
+        expect(validateAddress('город Уфа дом 3 кв 10', createElem('div', 'test'))).toBeFalsy();
+    });
+    it('should return true if address is valid', () => {
+        expect(validateAddress('город москва улица острякова дом 8', createElem('div', 'test'))).toBeTruthy();
+    });
+    it('should be case insensitive', () => {
+        expect(validateAddress('гОРОд  МОсквА улИца ОСТРЯКОВА  доМ 8', createElem('div', 'test'))).toBeTruthy();
+    });
+    it('should accept only russian letters and other symbols', () => {
+        expect(validateAddress('Moscow, street Ostryakova house 8', createElem('div', 'test'))).toBeFalsy();
+    });
+});
