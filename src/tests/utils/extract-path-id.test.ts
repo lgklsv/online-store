486d76dc2ee5d5a3e259e06045d6a7f59bf6b3f4
lgklsv <kolosov20011@gmail.com>
2023-01-07 17:24:31 +0300

    feat: add tests for the fucntion that gets the products stock values

--- src/tests/utils/extract-path-id.test.ts
+++ src/tests/utils/extract-path-id.test.ts
@@ -4,12 +4,13 @@ describe('extractPathId', () => {
     it('should return path id', () => {
         expect(extractPathId('/product/1')).toBe(1);
     });
-    it('should return -1 if there is no id or id', () => {
+    it('should return -1 if there is no id or id is not valid', () => {
         expect(extractPathId('/product/ytfyc')).toBe(-1);
         expect(extractPathId('/product/')).toBe(-1);
     });
     it('should return -1 if path is not /product/:id', () => {
         expect(extractPathId('/product/ytfyc/1')).toBe(-1);
         expect(extractPathId('/product/ytfyc/frfw/1')).toBe(-1);
+        expect(extractPathId('/product/ytfyc/frfw/iwefh/1')).toBe(-1);
     });
 });
