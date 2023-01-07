a10c8fee5cdb0c18722d2075ef219030863f3088
Alesia <alesia.litvinenko@gmail.com>
2023-01-08 00:02:32 +0300

    feat: add test calcDiscount

---
+++ src/tests/utils/calculate-amount-cart.test.ts
@@ -0,0 +1,16 @@
+import { calcDiscount } from '../../utils/calculate-amount-cart';
+
+describe('Function calcDiscount', () => {
+    it('should return string data with correctly formatted price - 1089 ₽', () => {
+        const calc = calcDiscount(1233, 12);
+
+        expect(typeof calc).toEqual('string');
+        expect(calc).toBe('1 089 ₽');
+    });
+    it('should handle incorrect price and discount', () => {
+        expect(calcDiscount(0, 12)).toEqual('0 ₽');
+        expect(calcDiscount(-1, 12)).toEqual('0 ₽');
+        expect(calcDiscount(192792814, -24)).toEqual('192 792 814 ₽');
+        expect(calcDiscount(-1, -1)).toEqual('0 ₽');
+    });
+});
--- src/utils/get-cart-page.ts
+++ src/utils/get-cart-page.ts
@@ -1,4 +1,4 @@
-export const getCartPage = (cartItems: CartData[], page: number, limit: number) => {
+export const getCartPage = (cartItems: CartData[], page: number, limit: number): CartData[] => {
     const start = (page - 1) * limit;
     const end = page * limit;
     return cartItems.slice(start, end);
